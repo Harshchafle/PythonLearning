4e9fb310f904a9f5381e074c00b42ca1066e1d2a
Harsh Chafle <157059028+Harshchafle@users.noreply.github.com>
2024-05-26 08:22:53 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,116 +1,113 @@
 
-# print(int(False))
+print(int(False))
 
-# print('1'+'2')
+print('1'+'2')
 
-# myvar = 'hello'
-# print(myvar.upper())
+myvar = 'hello'
+print(myvar.upper())
 
-# print(str(1+1))
+print(str(1+1))
 
-# print( "123".replace("12", "ab") )
+print( "123".replace("12", "ab") )
 
-# x=2/2
-# print(type(x))
+x=2/2
+print(type(x))
 
-# say_what=('say',' what', 'you', 'will') 
-# print(say_what[-1])
+say_what=('say',' what', 'you', 'will') 
+print(say_what[-1])
 
-# A=(1,2,3,4,5)
-# print(A[1:4])
+A=(1,2,3,4,5)
+print(A[1:4])
 
-# A=(1,2,3,4,5)
-# print(len(A))
+A=(1,2,3,4,5)
+print(len(A))
 
-# B=[1,2,[3,'a'],[4,'b']]
-# print(B[3][1])
+B=[1,2,[3,'a'],[4,'b']]
+print(B[3][1])
 
-# print([1,2,3]+[1,1,1])
+print([1,2,3]+[1,1,1])
 
-# A = [1]
-# A.append([2,3,4,5])
-# print(len(A))
+A = [1]
+A.append([2,3,4,5])
+print(len(A))
 
-# print("HelloMike".split())
+print("HelloMike".split())
 
-# {"a":1,"b":2}
-# print()
+{"a":1,"b":2}
+print()
 
-# A=((11,12),[21,22])
-# print( A[1])
+A=((11,12),[21,22])
+print( A[1])
 
-# A=((1),[2,3],[4])
-# print(A[2][0])
+A=((1),[2,3],[4])
+print(A[2][0])
 
-# A=["hard rock",10,1.2]
-# del(A[0]) 
-# print(A)
+A=["hard rock",10,1.2]
+del(A[0]) 
+print(A)
 
-# print(len(("disco",10,1.2, "hard rock",10)))
+print(len(("disco",10,1.2, "hard rock",10)))
 
-# looping statements
-# for (x) in range(0,3):{
-#     print(x)
-# }
+looping statements
+for (x) in range(0,3):{
+    print(x)
+}
 
-# for x in ['A','B','C']:
-#   print(x+'A')
+for x in ['A','B','C']:
+  print(x+'A')
 
-# for i,x in enumerate(['A','B','C']):{
-#     print(i,x)
-# }
+for i,x in enumerate(['A','B','C']):{
+    print(i,x)
+}
 
-# print(len(['A','B',1]))
+print(len(['A','B',1]))
 
-# print(len([sum([0,0,1])]) )
+print(len([sum([0,0,1])]) )
 
-# L=[1,3,2]
-# print(sorted(L))
+L=[1,3,2]
+print(sorted(L))
+##################################################################################
 
-# x="Go"
-# if(x=="Go"):
-#   print('Go ')
-# else:
-#   print('Stop')
-# print('Mike')
+x="Go"
+if(x=="Go"):
+  print('Go ')
+else:
+  print('Stop')
+print('Mike')
 
-# x=0
-# while(x<2):
-#     print(x)
-#     x=x+1   
+x=0
+while(x<2):
+    print(x)
+    x=x+1  
+  ###############################################################################
 
-# class Points(object):
-#   def __init__(self,x,y):
-#     self.x=x
-#     self.y=y
-#   def print_point(self):
-#     print('x=',self.x,' y=',self.y)
-# p1=Points("A","B")
-# p1.print_point()
+class Points(object):
+  def __init__(self,x,y):
+    self.x=x
+    self.y=y
+  def print_point(self):
+    print('x=',self.x,' y=',self.y)
+p1=Points("A","B")
+p1.print_point()
 
-# for i,x in enumerate(['A','B','C']):
-#     print(i,2*x)
+for i,x in enumerate(['A','B','C']):
+    print(i,2*x)
+  ##############################################################################
 
-# class Points(object):
-#   def __init__(self,x,y):
-#     self.x=x
-#     self.y=y
-#   def print_point(self):
-#     print('x=',self.x,' y=',self.y)
-# p2=Points(1,2)
-# p2.x='A'
-# p2.print_point()
+class Points(object):
+  def __init__(self,x,y):
+    self.x=x
+    self.y=y
+  def print_point(self):
+    print('x=',self.x,' y=',self.y)
+p2=Points(1,2)
+p2.x='A'
+p2.print_point()
+################################################################################
 
-# def step(x):
-#     if x>0:
-#         y=1
-#     else:
-#         y=0
-#     return y
-
-
-# a = np.array([0,1])
-# b = np.array([1,0])
-# np.dot(a,b) 
-
-
+def step(x):
+    if x>0:
+        y=1
+    else:
+        y=0
+    return y
