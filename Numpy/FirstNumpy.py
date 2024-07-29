7286291b1f1ed9f59190d0a53d33a8a1d9101e31
Harsh Chafle <157059028+Harshchafle@users.noreply.github.com>
2024-07-29 10:35:28 +0000

    First Numpy Files basics (array creation).

---
+++ Numpy/FirstNumpy.py
@@ -0,0 +1,8 @@
+import numpy as np;
+
+arr1d = np.array([1, 2, 3, 4],np.int16);
+arr2d = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+print(arr1d);
+print(arr2d);
+print(arr1d.shape);
+print(arr1d.dtype);              
